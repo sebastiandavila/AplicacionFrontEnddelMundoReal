e124069416fca791a13b6f054720d7fb56de30bd
@sebas.davila.garcia <sebas.davila.garcia@gmail.com>
2021-04-24 22:21:16 -0500

    COmponente home

---
+++ src/components/Home/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Banner = ({ appName, token }) => {
+  if (token) {
+    return null;
+  }
+  return (
+    <div className="banner">
+      <div className="container">
+        <h1 className="logo-font">
+          {appName.toLowerCase()}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/components/Home/MainView.js
@@ -0,0 +1,67 @@
+import ArticleList from '../ArticleList';
+import React from 'react';
+import agent from '../../agent';
+import { connect } from 'react-redux';
+import { CHANGE_TAB } from '../../constants/actionTypes';
+
+const YourFeedTab = props => {
+  if (props.token) {
+    const clickHandler = ev => {
+      ev.preventDefault();
+      props.onTabClick('feed', agent.Articles.feed, agent.Articles.feed());
+    }
+
+    return (
+      <li className="nav-item">
+        <a  href=""
+            className={ props.tab === 'feed' ? 'nav-link active' : 'nav-link' }
+            onClick={clickHandler}>
+          Your Feed
+        </a>
+      </li>
+    );
+  }
+  return null;
+};
+
+
+const TagFilterTab = props => {
+  if (!props.tag) {
+    return null;
+  }
+
+  return (
+    <li className="nav-item">
+      <a href="" className="nav-link active">
+        <i className="ion-pound"></i> {props.tag}
+      </a>
+    </li>
+  );
+};
+
+const mapStateToProps = state => ({
+  ...state.articleList,
+  tags: state.home.tags,
+  token: state.common.token
+});
+
+const mapDispatchToProps = dispatch => ({
+  onTabClick: (tab, pager, payload) => dispatch({ type: CHANGE_TAB, tab, pager, payload })
+});
+
+const MainView = props => {
+  return (
+    <div className="col-md-9">
+      
+
+      <ArticleList
+        pager={props.pager}
+        articles={props.articles}
+        loading={props.loading}
+        articlesCount={props.articlesCount}
+        currentPage={props.currentPage} />
+    </div>
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainView);
